208099fec1b395b65e5fb1617f1ceb9d6afbb07f
Fayth969 <faithugbedaojone@gmail.com>
2025-01-06 13:13:47 +0100

    added labels

--- matplot_file.py
+++ matplot_file.py
@@ -3,6 +3,9 @@ import numpy as np
 
 # x_axis = np.array([1, 15, 40, 20, 5])
 y_axis = np.array([10, 180, 15, 50, 4])
-plt.plot(y_axis, marker='*')
+plt.plot(y_axis, marker = 'o', linestyle='dashed', color='black')
 #print(dir(np)) # this line prints all the data of a module
+plt.title("REAL TIME ANALYSIS")
+plt.xlabel('x Axis')
+plt.ylabel('y Axis')
 plt.show()
