0b5ee43cf6fc84d29bccec50ddcbc9c0bb68a3b3
Fayth969 <faithugbedaojone@gmail.com>
2025-01-06 12:33:01 +0100

    maker ref

---
+++ matplot_file.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# x_axis = np.array([1, 15, 40, 20, 5])
+y_axis = np.array([10, 180, 15, 50, 4])
+plt.plot(y_axis, marker='*')
+#print(dir(np)) # this line prints all the data of a module
+plt.show()
