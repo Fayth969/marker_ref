866d91614a249d2b2dfb29968cc22e5d43baf4fe
Fayth969 <faithugbedaojone@gmail.com>
2025-01-13 09:59:12 +0100

    added list works

---
+++ fayth_trial.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+xaxis = np.array([100, 40, 80, 1])
+yaxis = np.array([500, 15, 25, 44])
+plt.plot(xaxis,yaxis, marker = 'o', ls = 'dashdot', color = 'red' python fayth_trial.py) #fmt standard
+plt.show()
